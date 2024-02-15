f280d0d56cc67bc0b305cf854d5e05160cbb4e6e
Joshua Bon <joshuajbon@gmail.com>
2024-02-15 15:43:58 +0100

    parametrise

--- src/transforms/cholesky-affine.jl
+++ src/transforms/cholesky-affine.jl
@@ -1,31 +1,31 @@
 include("pd-chol-helper.jl")
 
-mutable struct CholeskyAffine{N} <: Transform{N}
-    L::LowerTriangular{<:Real, Matrix{<:Real}}
-    b::Vector{<:Real}
-    function CholeskyAffine(L::LowerTriangular{T, Matrix{T}}, b::Vector{T}) where T<:Real
+mutable struct CholeskyAffine{N,T<:Real} <: Transform{N}
+    L::LowerTriangular{T, Matrix{T}}
+    b::Vector{T}
+    function CholeskyAffine(L::LowerTriangular{T, Matrix{T}}, b::Vector{T}) where {T<:Real}
         @assert length(b) == size(L, 1)
-        new{length(b)}(L, b)
+        new{length(b),T}(L, b)
     end
 end
 
-CholeskyAffine(vL::Vector{<:Real}, b::Vector{<:Real}) = CholeskyAffine(vec2chol(vL), b)
+CholeskyAffine(vL::Vector{T}, b::Vector{T}) where {T<:Real}  = CholeskyAffine(vec2chol(vL), b)
 CholeskyAffine(vLb::Vector{<:Real}, d::Int64) = CholeskyAffine(vLb[1:(end-d)], vLb[(end-d+1):end])
 function CholeskyAffine(d::Int64)
     par_zero = zeros(Int((d^2 + d)/2 + d))
     CholeskyAffine(par_zero, d)
 end
 
-dimension(::CholeskyAffine{N}) where {N} = (N,) # object dimension
-length(::CholeskyAffine{N}) where {N} = N # for internal use (within CholeskyAffine functions)
-nparam(::CholeskyAffine{N}) where {N} = N*(N + 3)/2 # number of params
+dimension(::CholeskyAffine{N,T}) where {N,T} = (N,) # object dimension
+length(::CholeskyAffine{N,T}) where {N,T} = N # for internal use (within CholeskyAffine functions)
+nparam(::CholeskyAffine{N,T}) where {N,T} = N*(N + 3)/2 # number of params
 
-function update!(chaf::CholeskyAffine, vL::Vector{<:Real}, b::Vector{<:Real})
+function update!(chaf::CholeskyAffine{N,T}, vL::Vector{T}, b::Vector{T}) where {N, T<:Real} 
     chaf = CholeskyAffine(vec2chol(vL), b)
     return chaf
 end
 
-function update!(chaf::CholeskyAffine, vLb::Vector{<:Real})
+function update!(chaf::CholeskyAffine{N,T}, vLb::Vector{T}) where {N, T<:Real} 
     n = length(chaf)
     return update!(chaf,  vec2chol(vLb[1:(end-n)]), vLb[(end-n+1):end])
 end
@@ -34,6 +34,6 @@ scaleparamvec(tf::CholeskyAffine) = chol2vec(tf.L)
 biasparamvec(tf::CholeskyAffine) = tf.b
 paramvec(tf::CholeskyAffine) = [chol2vec(tf.L); tf.b]
 
-(chaf::CholeskyAffine)(x::T, μs::T) where {T} = chaf.L * (x - μs) + μs + chaf.b 
+(chaf::CholeskyAffine{N,T})(x::T, μs::T) where {N,T<:Real} = chaf.L * (x - μs) + μs + chaf.b 
 
-(chaf::CholeskyAffine)(cal::Calibration{T}) where {T} = Calibration(cal.values, hcat([chaf.(clm, [cal.μs[i]]) for (i, clm) in enumerate(eachcol(cal.samples))]...))
+(chaf::CholeskyAffine{N,T})(cal::Calibration{T}) where {N,T<:Real} = Calibration(cal.values, hcat([chaf.(clm, [cal.μs[i]]) for (i, clm) in enumerate(eachcol(cal.samples))]...))
