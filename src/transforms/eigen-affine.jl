1aec5d8981fb7fd579de975c681b5f08b8126f07
Joshua Bon <joshuajbon@gmail.com>
2024-03-01 11:27:56 +0100

    Update vec2mat with dim argument

--- src/transforms/eigen-affine.jl
+++ src/transforms/eigen-affine.jl
@@ -11,10 +11,10 @@ mutable struct EigenAffine{N,T<:Real} <: Transform{N}
     end
 end
 
-EigenAffine(v::Vector{<:Real}, d::Int64) = EigenAffine(exp.(v[1:d]), vec2matposdiag(v[(d+1):(end-d)]), v[(end-d+1):end])
-function EigenAffine(d::Int64)
-    par_zero = zeros(Int( d * (d + 2) ))
-    EigenAffine(par_zero, d)
+EigenAffine(v::Vector{<:Real}, N::Int64) = EigenAffine(exp.(v[1:N]), vec2matposdiag(v[(N+1):(end-N)], N), v[(end-N+1):end])
+function EigenAffine(N::Int64)
+    par_zero = zeros(Int( N * (N + 2) ))
+    EigenAffine(par_zero, N)
 end
 
 
@@ -29,7 +29,7 @@ function update!(tf::EigenAffine{N,T}, d::Vector{T}, V::Matrix{T}, b::Vector{T})
     tf.b .= b
 end
 
-update!(tf::EigenAffine{N,T}, v::Vector{T}) where {N,T<:Real} = update!(tf, exp.(v[1:d]), vec2matposdiag(v[(d+1):(end-d)]), v[(end-d+1):end])
+update!(tf::EigenAffine{N,T}, v::Vector{T}) where {N,T<:Real} = update!(tf, exp.(v[1:N]), vec2matposdiag(v[(N+1):(end-N)], N), v[(end-N+1):end])
 
 maketransform(::EigenAffine{N,Ti}, v::Vector{To}) where {N,Ti<:Real,To<:Real} = EigenAffine(v, N)
 
@@ -37,7 +37,7 @@ idpenalty(tf::EigenAffine) = sum((tf.V * tf.V' - I) .^ 2) # for identifability o
 corrpenalty(tf::EigenAffine) = sum((tf.V - I) .^ 2)
 scalepenalty(tf::EigenAffine) = sum(tf.d .^ 2)
 
-paramvec(tf::EigenAffine) = [log.(tf.d); matposdiag2vec(tf.V); tf.b]
+paramvec(tf::EigenAffine{N,T}) where {N,T<:Real} = [log.(tf.d); matposdiag2vec(tf.V, N); tf.b]
 
 # for  (::EigenAffine) may not have same type as input (e.g. Float64 -> Dual)
 (tf::EigenAffine)(x::T, μs::T) where {T} = tf.V * ( tf.d .* (x - μs)) + μs + tf.b 
